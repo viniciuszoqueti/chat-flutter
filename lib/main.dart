41b1a01832f0e3a56b930248aec3b6de095520dc
viniciuszoqueti <32753769+viniciuszoqueti@users.noreply.github.com>
2020-09-02 23:21:51 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat/chat_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            iconTheme: IconThemeData(color: Colors.blue)),
+        home: ChatScreen());
+  }
+}
